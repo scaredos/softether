b3a11371b492519084a838fbac5d1355fbeb706a
Steven <scared@tuta.io>
2021-05-23 07:52:12 -0400

    Add documentation to example

--- python/examples/example.py
+++ python/examples/example.py
@@ -4,12 +4,15 @@ import softetherapi
 client = softetherapi.SoftEtherAPI(
     ip='0.0.0.0', port=443, hubname="VPN", password='Password')
 
+# authenticate() returns boolean based on status of authorization
 if client.authenticate():
     print("Successful Login")
 
+    # Example functions
     print(client.get_server_cert())
     print(client.get_server_info())
 
 else:
+    # Exit if not authenticated
     print("Login Failed!")
     exit(-1)
